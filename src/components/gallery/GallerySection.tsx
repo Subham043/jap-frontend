0681127cd97eb0c6b308e9d0991e1586080c4d1b
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-06-07 21:31:20 +0530

    fixed gallery api

--- src/components/gallery/GallerySection.tsx
+++ src/components/gallery/GallerySection.tsx
@@ -2,11 +2,19 @@ import React, { useCallback, useMemo, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import ImageViewer from 'react-simple-image-viewer';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { galleryData } from "./gallery-data";
 import Image from "next/image";
+import { axiosPublic } from "../../../axios";
+import { api_routes } from "@/helper/routes";
+import { GalleryCategoryState, GalleryState } from "@/helper/types";
+import useSWRInfinite from "swr/infinite";
+import useSWR from "swr";
+
+const galleryFetcher = async (url: string) => {
+  const res =await axiosPublic.get(url);
+  return res.data.data
+};
 
 const GallerySection = () => {
-    const [numberOfImage, setNumberOfImage] = useState(20);
     const [currentImage, setCurrentImage] = useState(0);
     const [isViewerOpen, setIsViewerOpen] = useState(false);
     const [tabIndex, setTabIndex] = useState(0);
@@ -16,22 +24,33 @@ const GallerySection = () => {
         setIsViewerOpen(true);
     }, []);
     
-    const fetchNextPage = useCallback(() => setNumberOfImage(prev => (prev + 20) < 102 ? prev + 20 : 102), []);
     
     const closeImageViewer = () => {
         setCurrentImage(0);
         setIsViewerOpen(false);
     };
 
-    const images = useMemo(()=>{
-        const data = galleryData.filter((item, i)=>{
-            if(tabIndex!==0){
-                return i+1<=numberOfImage && item.type===tabIndex;
-            }
-            return i+1<=numberOfImage;
-        });
-        return data;
-    },[numberOfImage, tabIndex])
+    const { data:galleryCategoryData } = useSWR<GalleryCategoryState[]>(`${api_routes.gallery_category}?total=20&page=1&sort=id`, galleryFetcher);
+
+    const getGalleryKey = useCallback((pageIndex:any, previousPageData:any) => {
+        if (previousPageData && previousPageData.length===0) return null;
+        return `${api_routes.gallery}?total=12&page=${pageIndex+1}&sort=-id${galleryCategoryData && galleryCategoryData.length>0 && (tabIndex-1)!==-1 ? '&filter[has_categories]='+galleryCategoryData[(tabIndex-1)].id : ''}`;
+    }, [galleryCategoryData, tabIndex])
+    
+    const {
+            data,
+            size:gallerySize,
+            setSize:setGallerySize,
+            isLoading:isGalleryLoading
+    } = useSWRInfinite<GalleryState>((pageIndex, previousPageData)=>getGalleryKey(pageIndex, previousPageData), galleryFetcher, {
+            initialSize:1,
+            revalidateAll: false,
+            revalidateFirstPage: false,
+            persistSize: false,
+            parallel: false
+    });
+
+    const fetchNextPage = () => setGallerySize(gallerySize+1);
 
     return (
         <section className="bd-team__area pt-55 pb-15">
@@ -44,49 +63,51 @@ const GallerySection = () => {
                             >
                             All
                             </Tab>
-                            <Tab
-                            className="nav-item nav-link show"
-                            >
-                            Farmer
-                            </Tab>
-                            <Tab
-                            className="nav-item nav-link show"
-                            >
-                            Events
-                            </Tab>
-                            <Tab
-                            className="nav-item nav-link show"
-                            >
-                            Marketing
-                            </Tab>
+                            {
+                                galleryCategoryData?.map((item, index) => (
+                                    <Tab
+                                    key={index}
+                                    className="nav-item nav-link show"
+                                    >
+                                        {item.name}
+                                    </Tab>
+                                ))
+                            }
                         </TabList>
                     </nav>
                     <div>
                         <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
-                        <TabPanel></TabPanel>
+                        {
+                            galleryCategoryData?.map((_, index) => (
+                                <TabPanel key={index}></TabPanel>
+                            ))
+                        }
                     </div>
                 </Tabs>
                 <InfiniteScroll
-                    dataLength={numberOfImage}
+                    dataLength={data ? data.flat().length : 0}
                     next={fetchNextPage}
                     hasMore={true}
-                    loader={null}
+                    loader={(isGalleryLoading) && <div className="text-center py-1">
+                        <div className="spinner-border text-success" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    </div>}
                     refreshFunction={fetchNextPage}
                     style={{overflowX: 'hidden'}}
                 >
                     <div className="row p-relative">
-                        {images.map((item, i) => {
+                        {data && data.flat().map((item, i) => {
                             return (
                             <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12" key={i+1}>
                                 <div className="bd-team__wrapper mx-1">
                                     <div className="bd-team__thumb w-img p-relative">
                                         <Image
+                                            priority={false}
                                             width={300}
                                             height={300}
                                             style={{ width: "100%", height: "250px", objectFit: 'cover', cursor: 'pointer' }}
-                                            src={`/assets/img/gallery/${item.id}.jpeg`}
+                                            src={item.image_link}
                                             alt="team-thumb"
                                             onClick={ () => openImageViewer(i) }
                                         />
@@ -99,7 +120,7 @@ const GallerySection = () => {
                 </InfiniteScroll>
                 {isViewerOpen && (
                     <ImageViewer
-                    src={images.map((item) => `/assets/img/gallery/${item.id}.jpeg`)}
+                    src={data ? data.flat().map((item) => item.image_link) : []}
                     currentIndex={ currentImage }
                     disableScroll={ true }
                     closeOnClickOutside={ true }
